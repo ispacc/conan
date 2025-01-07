c6b7601ddaf16c4d5e89b53b60d5b56d5d14377c
十四 <yh.xion@outlook.com>
2025-01-07 19:03:55 +0800

    update

--- conan-admin/src/main/java/io/origins/conanadmin/config/WxConfigOperator.java
+++ conan-admin/src/main/java/io/origins/conanadmin/config/WxConfigOperator.java
@@ -1,24 +1,24 @@
-package io.origins.conanadmin.config;
-
-import cn.binarywang.wx.miniapp.api.WxMaService;
-import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
-import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class WxConfigOperator {
-    private final WxConfigProperties wxConfigProperties;
-
-    @Bean
-    public WxMaService wxMaService() {
-        WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
-        config.setAppid(wxConfigProperties.getAppid());
-        config.setSecret(wxConfigProperties.getAppsecret());
-        WxMaService service = new WxMaServiceImpl();
-        service.setWxMaConfig(config);
-        return service;
-    }
-}
+//package io.origins.conanadmin.config;
+//
+//import cn.binarywang.wx.miniapp.api.WxMaService;
+//import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+//import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class WxConfigOperator {
+//    private final WxConfigProperties wxConfigProperties;
+//
+//    @Bean
+//    public WxMaService wxMaService() {
+//        WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
+//        config.setAppid(wxConfigProperties.getAppid());
+//        config.setSecret(wxConfigProperties.getAppsecret());
+//        WxMaService service = new WxMaServiceImpl();
+//        service.setWxMaConfig(config);
+//        return service;
+//    }
+//}
